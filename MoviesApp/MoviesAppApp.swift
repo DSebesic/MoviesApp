addfa29605ad7df5041e742986cca0df16faf135
Flabbergast <flabbergast@FlabbergastsMBP.localdomain>
2024-03-04 14:28:29 +0100

    initial commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Flabbergast on 04.03.2024..
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
